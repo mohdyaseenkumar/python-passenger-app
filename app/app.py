7dc1e042a8f749a4f2edc4475f88d0333b5fbbe3
mohdyaseenkumar <53486897+mohdyaseenkumar@users.noreply.github.com>
2026-01-11 13:29:57 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,1 +1,16 @@
+# app/app.py
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route("/")
+    def index():
+        return "Hello from Python on Passenger + Nginx!"
+
+    @app.route("/health")
+    def health():
+        return "ok", 200
+
+    return app
 
